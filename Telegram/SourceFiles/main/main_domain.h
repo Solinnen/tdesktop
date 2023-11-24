ae96b69571c32f07ef21e85771c8d6172a39e52e
Solinnen <solinnen@proton.me>
2023-11-24 20:30:03 +0200

    a

--- Telegram/SourceFiles/main/main_domain.h
+++ Telegram/SourceFiles/main/main_domain.h
@@ -30,8 +30,8 @@ public:
 		std::unique_ptr<Account> account;
 	};
 
-	static constexpr auto kMaxAccounts = 30;
-	static constexpr auto kPremiumMaxAccounts = 60;
+	static constexpr auto kMaxAccounts = 10;
+	static constexpr auto kPremiumMaxAccounts = 20;
 
 	explicit Domain(const QString &dataName);
 	~Domain();
